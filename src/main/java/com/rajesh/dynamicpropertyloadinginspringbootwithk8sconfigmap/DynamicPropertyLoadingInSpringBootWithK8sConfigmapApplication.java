2149305f20828297a540bbe2d88e702882e69609
Rajesh <prrajesh@deloitte.com>
2023-01-27 21:44:02 +0530

    first commit

---
+++ src/main/java/com/rajesh/dynamicpropertyloadinginspringbootwithk8sconfigmap/DynamicPropertyLoadingInSpringBootWithK8sConfigmapApplication.java
@@ -0,0 +1,13 @@
+package com.rajesh.dynamicpropertyloadinginspringbootwithk8sconfigmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicPropertyLoadingInSpringBootWithK8sConfigmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicPropertyLoadingInSpringBootWithK8sConfigmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rajesh/dynamicpropertyloadinginspringbootwithk8sconfigmap/DynamicPropertyLoadingInSpringBootWithK8sConfigmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajesh.dynamicpropertyloadinginspringbootwithk8sconfigmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicPropertyLoadingInSpringBootWithK8sConfigmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
